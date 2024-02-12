19f608bdd0b051f34a0a0220801a189e26e0c076
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-02-12 18:27:00 -0300

    Configurando pytest

--- passagens/tests.py
+++ passagens/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class PassageTestCase(TestCase):
+    def test_the_pytest_is_ok(self):
+        print('Teste de passagem')
+        assert 1==1
